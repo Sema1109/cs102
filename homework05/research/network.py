eab25b16abd9d1f54e9aa21ef0780937c79b636b
Sema1109 <91030716+Sema1109@users.noreply.github.com>
2022-03-21 22:26:36 +0300

    Update network.py

--- homework05/research/network.py
+++ homework05/research/network.py
@@ -9,7 +9,7 @@ from vkapi.friends import get_friends, get_mutual
 
 
 def ego_network(
-        user_id: tp.Optional[int] = None, friends: tp.Optional[tp.List[int]] = None
+    user_id: tp.Optional[int] = None, friends: tp.Optional[tp.List[int]] = None
 ) -> tp.List[tp.Tuple[int, int]]:
     """
     Построить эгоцентричный граф друзей.
@@ -57,9 +57,9 @@ def get_communities(net: tp.List[tp.Tuple[int, int]]) -> tp.Dict[int, tp.List[in
 
 
 def describe_communities(
-        clusters: tp.Dict[int, tp.List[int]],
-        friends: tp.List[tp.Dict[str, tp.Any]],
-        fields: tp.Optional[tp.List[str]] = None,
+    clusters: tp.Dict[int, tp.List[int]],
+    friends: tp.List[tp.Dict[str, tp.Any]],
+    fields: tp.Optional[tp.List[str]] = None,
 ) -> pd.DataFrame:
     if fields is None:
         fields = ["first_name", "last_name"]
@@ -77,4 +77,5 @@ def describe_communities(
 if __name__ == "__main__":
     net = ego_network()
     print(net)
+        
     plot_ego_network(net)
