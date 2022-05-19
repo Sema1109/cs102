1629e59b0dfd2b951ac3da1ceb584ab463a2e8e4
Sema1109 <91030716+Sema1109@users.noreply.github.com>
2022-05-19 17:52:16 +0300

    Homework06

--- homework06/db.py
+++ homework06/db.py
@@ -1,15 +1,14 @@
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+from scraputils import get_news  # type: ignore
+from sqlalchemy import Column, Integer, String, create_engine  # type: ignore
+from sqlalchemy.ext.declarative import declarative_base  # type: ignore
+from sqlalchemy.orm import sessionmaker  # type: ignore
 
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
 session = sessionmaker(bind=engine)
 
 
-class News(Base):
+class News(Base):  # type: ignore
     __tablename__ = "news"
     id = Column(Integer, primary_key=True)
     title = Column(String)
@@ -19,4 +18,19 @@ class News(Base):
     points = Column(Integer)
     label = Column(String)
 
+
 Base.metadata.create_all(bind=engine)
+
+def fill_db(n_pages=10):
+    s = session()
+    news_list = get_news("https://news.ycombinator.com/newest", n_pages)
+    for n in news_list:
+        news = News(
+            title=n["title"],
+            author=n["author"],
+            url=n["url"],
+            comments=n["comments"],
+            points=n["points"],
+        )
+        s.add(news)
+    s.commit()
